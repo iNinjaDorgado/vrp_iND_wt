c902e7df33163c130290a35eadf50b1ae558325f
iNinjaDorgado <54279244+iNinjaDorgado@users.noreply.github.com>
2021-07-15 19:23:06 -0300

    Update client.lua

--- vrp_iND_wt-vrpex/client.lua
+++ vrp_iND_wt-vrpex/client.lua
@@ -1,12 +1,8 @@
-local Tunnel = module("vrp","lib/Tunnel")
-local Proxy = module("vrp","lib/Proxy")
-vRP = Proxy.getInterface("vRP")
-
 RegisterCommand("cor",function(source,args)
-	local tinta = parseInt(args[1])
+	local tinta = tonumber(args[1])
 	local ped = PlayerPedId()
 	local arma = GetSelectedPedWeapon(ped)
 	if tinta >= 0 then
-		SetPedWeaponTintIndex(ped,arma,tinta)
+		SetPedWeaponTintIndex(ped, arma, tinta)
 	end
-end,false)
+end, false)
